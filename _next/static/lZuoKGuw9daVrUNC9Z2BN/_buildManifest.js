3a9918d1b4a77cec88fadb6707cf816a203eb274
putrasatr <satriaputrapratama2002@gmail.com>
2022-06-18 15:22:23 +0700

    Initial Commit

---
+++ _next/static/lZuoKGuw9daVrUNC9Z2BN/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/1bfc9850-8cdf22d09c5e3393.js","static/chunks/pages/index-d8e09bb576138102.js"],"/_error":["static/chunks/pages/_error-0a004b8b8498208d.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
