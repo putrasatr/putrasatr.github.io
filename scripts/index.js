f429d0622f2990dfdda3d912db012e31776b40aa
putrasatr <satriaputrapratama2002@gmail.com>
2022-11-08 15:48:34 +0700

    fix importing

--- scripts/index.js
+++ scripts/index.js
@@ -1,26 +1,5 @@
-// const home = `<p>I started from a bootcamp, learned basic javascript and then took on an unusual challenge,
-// working to
-// create an E-commerce web, CMS, and creative dev, work in a great company and keep learning.</p>`;
-// const contact = `<p>I am Contact Page</p>`;
-// const about = `<p>I am About Page</p>`;
-
-// const routes = {
-//   "/index.html": home,
-//   "/": home,
-//   "/contact": contact,
-//   "/about": about,
-// };
-
-// const rootDiv = document.getElementById("root");
-// rootDiv.innerHTML = routes[window.location.pathname];
 let selectedTab = 0;
 
-const onNavigate = (pathname) => {
-  console.log(pathname);
-  // pathname = "/";
-  // window.history.pushState({}, pathname, window.location.origin + pathname);
-  // rootDiv.innerHTML = routes[pathname];
-};
 
 function backRoute() {
   if (window.location.pathname !== "/index.html") {
